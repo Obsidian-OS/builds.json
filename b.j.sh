68a981a37d02d79889f3d0160f8e5e2c7b4974ac
/home/odd <odd@obsidianos.xyz>
2025-11-21 18:19:06 -0500

    Update b.j.sh

--- b.j.sh
+++ b.j.sh
@@ -9,12 +9,17 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 EOF
 # Check dependencies
-for cmd in curl gpg kitten jq; do
+for cmd in curl gpg jq; do
   if ! command -v "$cmd" >/dev/null 2>&1; then
     echo "You need $cmd."
     exit 1
   fi
 done
+if ! command -v kitten > /dev/null 2>&1; then
+  kitten() {
+    return 1
+  }
+fi
 # Download builds.json
 : "${root:=https://files.obsidianos.xyz}"
 : "${builds:=${root}/builds.json}"
